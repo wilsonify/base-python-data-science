59db9ec5ff431e09bb4e56df1c82af125e7b17cf
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-09 11:48:31 -0500

    resolve int/float type check

--- src/data-plots/data_plots_from_scratch/charts.py
+++ src/data-plots/data_plots_from_scratch/charts.py
@@ -1,5 +1,15 @@
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+
+def plot_normal_pdfs(plt):
+    xs = [x / 10.0 for x in range(-50, 50)]
+    plt.plot(xs, [normal_pdf(x, sigma=1.0) for x in xs], "-", label="mu=0,sigma=1")
+    plt.plot(xs, [normal_pdf(x, sigma=2.0) for x in xs], "--", label="mu=0,sigma=2")
+    plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ":", label="mu=0,sigma=0.5")
+    plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], "-.", label="mu=-1,sigma=1")
+    plt.legend()
+    plt.show()
 
 
 def recolor_image(input_file, k=5):
--- src/data-scratch/data_science_from_scratch/probability.py
+++ src/data-scratch/data_science_from_scratch/probability.py
@@ -25,18 +25,8 @@ def normal_pdf(x, mu=0, sigma=1):
     return math.exp(-(x - mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma)
 
 
-def plot_normal_pdfs(plt):
-    xs = [x / 10.0 for x in range(-50, 50)]
-    plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], "-", label="mu=0,sigma=1")
-    plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], "--", label="mu=0,sigma=2")
-    plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ":", label="mu=0,sigma=0.5")
-    plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], "-.", label="mu=-1,sigma=1")
-    plt.legend()
-    plt.show()
-
-
-def normal_cdf(x, mu=0, sigma=1):
-    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+def normal_cdf(x, mu=0.0, sigma=1.0):
+    return (1.0 + math.erf((x - mu) / math.sqrt(2.0) / sigma)) / 2.0
 
 
 def plot_normal_cdfs(plt):
