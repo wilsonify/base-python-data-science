864eee1eb79303a362bcd54ebafbde3062aa3b4d
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-10 15:33:20 -0500

    data is no longer in src

--- src/data-scratch/data_science_from_scratch/config.py
+++ src/data-scratch/data_science_from_scratch/config.py
@@ -9,7 +9,7 @@ import os
 AMQP_HOST = "10.1.1.16"
 AMQP_PORT = 5672
 HOME_DIR = os.path.expanduser("~")
-LOCAL_DATA_DIR = os.path.join(HOME_DIR, "repos/base-python-data-science/src/tests/data")
+LOCAL_DATA_DIR = os.path.join(HOME_DIR, "repos/base-python-data-science/tests/data")
 LOGGING_LEVEL = logging.DEBUG
 LOGGING_CONFIG_DICT = dict(
     version=1,
