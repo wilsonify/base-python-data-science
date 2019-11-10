6d02428efdfc91a78feb0324beb4bb2577688567
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-10 15:46:28 -0500

    create tests for libraries

---
+++ tests/test_data_science_from_scratch/test_library/test_probability.py
@@ -0,0 +1,12 @@
+import logging
+import os
+
+import pytest
+from data_science_from_scratch.library import probability
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.join(current_dir, os.pardir)
+
+
+def test_smoke():
+    logging.info("is anything on fire")
---
+++ tests/test_data_science_from_scratch/test_library/test_stats.py
@@ -0,0 +1,12 @@
+import logging
+import os
+
+import pytest
+from data_science_from_scratch.library import stats
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.join(current_dir, os.pardir)
+
+
+def test_smoke():
+    logging.info("is anything on fire")
