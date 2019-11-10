e117e2ba728ea4d26917ab2f541bda1466631d1b
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-10 09:04:44 -0500

    log to console only

--- src/data-scratch/data_science_from_scratch/config.py
+++ src/data-scratch/data_science_from_scratch/config.py
@@ -1,24 +1,21 @@
+"""
+configuration
+these are intended to be imported by many modules
+"""
+
 import logging
-import os
 
-host = "10.1.1.16"
-port = 5672
+AMQP_HOST = "10.1.1.16"
+AMQP_PORT = 5672
 
-logging_dir = "logs"
-logging_level = logging.DEBUG
-logging_file_name = "base-python-data-science.log"
-logging_config_dict = dict(
+LOGGING_LEVEL = logging.DEBUG
+LOGGING_CONFIG_DICT = dict(
     version=1,
     formatters={
-        "simple": {"format": """%(asctime)s %(name)-12s %(levelname)-8s %(message)s"""}
+        "simple": {
+            "format": """%(asctime)s | %(name)s | %(lineno)s | %(levelname)s | %(message)s"""
+        }
     },
-    handlers={
-        "console": {"class": "logging.StreamHandler", "formatter": "simple"},
-        "file": {
-            "class": "logging.FileHandler",
-            "filename": os.path.join(logging_dir, logging_file_name),
-            "formatter": "simple",
-        },
-    },
-    root={"handlers": ["console", "file"], "level": logging_level},
+    handlers={"console": {"class": "logging.StreamHandler", "formatter": "simple"}},
+    root={"handlers": ["console"], "level": logging.DEBUG},
 )
