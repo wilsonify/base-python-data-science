eaf3ee9d92b104cca42aabc046967ea3f1580e15
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-10 09:45:25 -0500

    refactor main()

--- src/data-scratch/data_science_from_scratch/egrep.py
+++ src/data-scratch/data_science_from_scratch/egrep.py
@@ -1,18 +1,28 @@
-# egrep.py
+"""
+If you run your Python scripts at the command line,
+you can pipe data through them using sys.stdin and sys.stdout.
+reads in lines of text and spit back the ones that match a regular expression
+"""
+
 import re
 import sys
 from logging.config import dictConfig
 
 from data_science_from_scratch import config
 
-if __name__ == "__main__":
-    dictConfig(config.logging_config_dict)  # sys.argv is the list of command-line arguments
-    # sys.argv[0] is the name of the program itself
-    # sys.argv[1] will be the regex specfied at the command line
-    regex = sys.argv[1]
 
-    # for every line passed into the script
+def main(regex):
+    """
+    reads in lines of text and spit back the ones that match a regular expression
+    :param regex:
+    :return:
+    """
     for line in sys.stdin:
-        # if it matches the regex, write it to stdout
         if re.search(regex, line):
             sys.stdout.write(line)
+
+
+if __name__ == "__main__":
+    dictConfig(config.LOGGING_CONFIG_DICT)
+    regex = sys.argv[1] if len(sys.argv) > 1 else ""  # sys.argv[0] is the name of the program itself
+    main(regex)
