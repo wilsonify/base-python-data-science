ecaf6e3ed19d9a33e0bc43b92612ea15f5cbb88d
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-10 20:39:10 -0500

    create place holder for tests

--- tests/test_data_science_from_scratch/test_library/test_linear_algebra.py
+++ tests/test_data_science_from_scratch/test_library/test_linear_algebra.py
@@ -1,7 +1,6 @@
 import logging
 import os
 
-import pytest
 from data_science_from_scratch.library import linear_algebra
 
 current_dir = os.path.dirname(__file__)
@@ -10,3 +9,79 @@ parent_dir = os.path.join(current_dir, os.pardir)
 
 def test_smoke():
     logging.info("is anything on fire")
+
+
+def test_distance():
+    linear_algebra.distance()
+
+
+def test_dot():
+    linear_algebra.dot()
+
+
+def test_get_column():
+    linear_algebra.get_column()
+
+
+def test_get_row():
+    linear_algebra.get_row()
+
+
+def test_identity_matrix():
+    linear_algebra.identity_matrix()
+
+
+def test_is_diagonal():
+    linear_algebra.is_diagonal()
+
+
+def test_magnitude():
+    linear_algebra.magnitude()
+
+
+def test_make_matrix():
+    linear_algebra.make_matrix()
+
+
+def test_math():
+    linear_algebra.math()
+
+
+def test_matrix_add():
+    linear_algebra.matrix_add()
+
+
+def test_reduce():
+    linear_algebra.reduce()
+
+
+def test_scalar_multiply():
+    linear_algebra.scalar_multiply()
+
+
+def test_shape():
+    linear_algebra.shape()
+
+
+def test_squared_distance():
+    linear_algebra.squared_distance()
+
+
+def test_sum_of_squares():
+    linear_algebra.sum_of_squares()
+
+
+def test_vector_add():
+    linear_algebra.vector_add()
+
+
+def test_vector_mean():
+    linear_algebra.vector_mean()
+
+
+def test_vector_subtract():
+    linear_algebra.vector_subtract()
+
+
+def test_vector_sum():
+    linear_algebra.vector_sum()
