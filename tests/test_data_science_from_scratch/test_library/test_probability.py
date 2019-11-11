94d43d8d4f69e81788e87432f761ecebd473b853
wilsonify <tom.andrew.wilson@gmail.com>
2019-11-10 20:39:24 -0500

    create place holder for tests

--- tests/test_data_science_from_scratch/test_library/test_probability.py
+++ tests/test_data_science_from_scratch/test_library/test_probability.py
@@ -1,7 +1,6 @@
 import logging
 import os
 
-import pytest
 from data_science_from_scratch.library import probability
 
 current_dir = os.path.dirname(__file__)
@@ -10,3 +9,59 @@ parent_dir = os.path.join(current_dir, os.pardir)
 
 def test_smoke():
     logging.info("is anything on fire")
+
+
+def test_bernoulli_trial():
+    probability.bernoulli_trial()
+
+
+def test_binomial():
+    probability.binomial()
+
+
+def test_config():
+    probability.config()
+
+
+def test_dictConfig():
+    probability.dictConfig()
+
+
+def test_inverse_normal_cdf():
+    probability.inverse_normal_cdf()
+
+
+def test_logging():
+    probability.logging()
+
+
+def test_math():
+    probability.math()
+
+
+def test_normal_cdf():
+    probability.normal_cdf()
+
+
+def test_normal_pdf():
+    probability.normal_pdf()
+
+
+def test_plot_normal_cdfs():
+    probability.plot_normal_cdfs()
+
+
+def test_random():
+    probability.random()
+
+
+def test_random_kid():
+    probability.random_kid()
+
+
+def test_uniform_cdf():
+    probability.uniform_cdf()
+
+
+def test_uniform_pdf():
+    probability.uniform_pdf()
