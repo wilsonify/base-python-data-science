f58587a6e5ade7972ff602de0551b29a213bc315
wilsonify <tom.a.wilson@gmail.com>
2022-06-22 16:28:51 -0400

    use json-ref-dict

--- src/rest-scratch/openapi_server/__main__.py
+++ src/rest-scratch/openapi_server/__main__.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
+import os
 import connexion
-
+from json_ref_dict import RefDict, materialize
 from openapi_server import encoder
 
+path_to_here = os.path.abspath(os.path.dirname(__file__))
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
+    openapi_dict = RefDict(f"{path_to_here}/openapi/openapi.yaml")
+    app = connexion.App(__name__, specification_dir="./openapi/")
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Swagger Petstore'},
-                pythonic_params=True)
+    app.add_api(
+        openapi_dict,
+        arguments={"title": "Swagger Petstore"}, 
+        pythonic_params=True
+    )
 
     app.run(port=8080)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
