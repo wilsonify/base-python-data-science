e99ba17bd0ceb91cc8c102bc866b117e75e79466
wilsonify <tom.a.wilson@gmail.com>
2022-06-25 11:49:16 -0400

    add remote procedure calls

--- src/rest-scratch/openapi_server/controllers/math_controller.py
+++ src/rest-scratch/openapi_server/controllers/math_controller.py
@@ -1,8 +1,8 @@
 import logging
-import math
 from typing import Tuple
 
 from openapi_server.models import SqrtInput, SqrtOutput, StrengthInput, StrengthOutput
+from openapi_server.rpc import RemoteProcedure
 
 
 def mysqrt(body: dict) -> Tuple[dict, int]:  # noqa: E501
@@ -10,25 +10,31 @@ def mysqrt(body: dict) -> Tuple[dict, int]:  # noqa: E501
     logging.debug(f"body = {body}")
     logging.debug(f"type(body) = {type(body)}")
     body_in = SqrtInput().from_dict(body)
-    x = body_in.x
-    sqrt_output = SqrtOutput(
-        x=x,
-        result=math.sqrt(x)
-    )
+    body_in_dict = body_in.to_dict()
+    body_in_dict['strategy'] = "mysqrt"
+    rpc = RemoteProcedure(routing_key='dsfs')
+    response_body = rpc.call(body_in_dict)
+    status_code = response_body['status_code']
+    sqrt_output = SqrtOutput().from_dict(response_body)
     body_out = sqrt_output.to_dict()
-    return body_out, 200
+    return body_out, status_code
 
 
 def mystrength(body: dict) -> Tuple[dict, int]:  # noqa: E501
     """ signal strength """
     body_in = StrengthInput().from_dict(body)
+    body_in_dict = body_in.to_dict()
+    body_in_dict['strategy'] = "mystrength"
     actual = body_in.actual
     expected = body_in.expected
-    strength = actual / expected
+    rpc = RemoteProcedure(routing_key='dsfs')
+    response_body = rpc.call(body_in_dict)
+    status_code = response_body['status_code']
+    strength = response_body["strength"]
     out_so = StrengthOutput(
         actual=actual,
         expected=expected,
         strength=strength
     )
     out_dict = out_so.to_dict()
-    return out_dict, 200
+    return out_dict, status_code
