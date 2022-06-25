85db08bc174ce860d1612f79bce868ebc205db79
wilsonify <tom.a.wilson@gmail.com>
2022-06-25 07:58:58 -0400

    done use Pet in math

--- src/rest-scratch/openapi_server/controllers/math_controller.py
+++ src/rest-scratch/openapi_server/controllers/math_controller.py
@@ -1,16 +1,17 @@
+import math
+
 import connexion
 import six
-
-from openapi_server.models.api_response import ApiResponse  # noqa: E501
-from openapi_server.models.pet import Pet  # noqa: E501
 from openapi_server import util
-import math
+from openapi_server.models.api_response import ApiResponse  # noqa: E501
+from openapi_server.models.sqrt_input import SqrtInput
+from openapi_server.models.strength_input import StrengthInput
 
 
 def mysqrt(body):  # noqa: E501
     """ square root """
     if connexion.request.is_json:
-        body = Pet.from_dict(connexion.request.get_json())  # noqa: E501
+        body = SqrtInput.from_dict(connexion.request.get_json())  # noqa: E501
 
     return dict(
         x=body['x'],
@@ -21,7 +22,7 @@ def mysqrt(body):  # noqa: E501
 def mystrength(body):  # noqa: E501
     """ signal strength """
     if connexion.request.is_json:
-        body = Pet.from_dict(connexion.request.get_json())  # noqa: E501
+        body = StrengthInput.from_dict(connexion.request.get_json())  # noqa: E501
     actual = body['actual']
     expected = body['expected']
     result = actual / expected
