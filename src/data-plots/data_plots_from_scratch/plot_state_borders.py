4a17143ec9be9560b4aecb8e000ce20ea9e373b0
wilsonify <tom.a.wilson@gmail.com>
2022-07-04 11:06:41 -0400

    reorg state_boarders as functions

--- src/data-plots/data_plots_from_scratch/plot_state_borders.py
+++ src/data-plots/data_plots_from_scratch/plot_state_borders.py
@@ -1,25 +1,36 @@
 import re
+
 import matplotlib.pyplot as plt
 
-segments = []
-points = []
 
-lat_long_regex = r"<point lat=\"(.*)\" lng=\"(.*)\""
-
-with open("states.txt", "r") as f:
-    lines = [line for line in f]
-
-for line in lines:
-    if line.startswith("</state>"):
-        for p1, p2 in zip(points, points[1:]):
-            segments.append((p1, p2))
-        points = []
-    s = re.search(lat_long_regex, line)
-    if s:
-        lat, lon = s.groups()
-        points.append((float(lon), float(lat)))
+def read_lines(fn="states.txt"):
+    with open(fn, "r") as f:
+        lines = [line for line in f]
+    return lines
 
 
-def plot_state_borders(color="0.8"):
+def lines_to_segments(lines):
+    lat_long_regex = r"<point lat=\"(.*)\" lng=\"(.*)\""
+    segments = []
+    points = []
+    for line in lines:
+        if line.startswith("</state>"):
+            for p1, p2 in zip(points, points[1:]):
+                segments.append((p1, p2))
+            points = []
+        s = re.search(lat_long_regex, line)
+        if s:
+            lat, lon = s.groups()
+            points.append((float(lon), float(lat)))
+    return segments
+
+
+def plot_state_borders(segments, color="0.8"):
     for (lon1, lat1), (lon2, lat2) in segments:
         plt.plot([lon1, lon2], [lat1, lat2], color=color)
+
+
+if __name__ == "__main__":
+    lns = read_lines(fn="states.txt")
+    sgmts = lines_to_segments(lns)
+    plot_state_borders(segments=sgmts)
