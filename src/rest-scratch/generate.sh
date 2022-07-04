66a077c8d3a01738517273b924be3916290db2c2
wilsonify <tom.a.wilson@gmail.com>
2022-07-04 10:35:56 -0400

    generate more models

--- src/rest-scratch/generate.sh
+++ src/rest-scratch/generate.sh
@@ -2,6 +2,6 @@ docker run --rm \
   -u $(id -u):$(id -g) \
   -v "${PWD}:/local" \
   openapitools/openapi-generator-cli:latest generate \
-  -i /local/openapi/openapi.yaml \
+  -i /local/openapi_server/openapi/openapi.yaml \
   -g python-flask \
   -o /local/rest_scratch
