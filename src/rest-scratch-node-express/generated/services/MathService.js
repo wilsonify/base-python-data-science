1a20e2eca3e4e20a9214e8a928e4062ce7c7bde1
wilsonify <tom.andrew.wilson@gmail.com>
2022-10-01 18:20:13 -0400

    implement math.sqrt

--- src/rest-scratch-node-express/generated/services/MathService.js
+++ src/rest-scratch-node-express/generated/services/MathService.js
@@ -671,7 +671,8 @@ const sqrt = ({ sqrtInput }) => new Promise(
   async (resolve, reject) => {
     try {
       resolve(Service.successResponse({
-        sqrtInput,
+        "x": x,
+        "result": Math.sqrt(sqrtInput.x)
       }));
     } catch (e) {
       reject(Service.rejectResponse(
