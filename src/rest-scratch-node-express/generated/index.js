7aaeeaf10b32f6ad5a6c9ee6aeb28d865b135dc3
wilsonify <tom.a.wilson@gmail.com>
2022-10-08 08:17:51 -0400

    log yaml path

--- src/rest-scratch-node-express/generated/index.js
+++ src/rest-scratch-node-express/generated/index.js
@@ -4,6 +4,8 @@ const ExpressServer = require('./expressServer');
 
 const launchServer = async () => {
   try {
+    console.log(`__dirname = ${__dirname}`)
+    console.log(`config.OPENAPI_YAML = ${config.OPENAPI_YAML}`)
     this.expressServer = new ExpressServer(config.URL_PORT, config.OPENAPI_YAML);
     this.expressServer.launch();
     logger.info('Express server running');
