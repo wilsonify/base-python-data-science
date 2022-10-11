3caa7c8d8dee1d50a50d7539255393d1b29906ee
wilsonify <tom.a.wilson@gmail.com>
2022-10-11 04:03:13 -0400

    simplify error handling

--- src/rest-scratch-node-express/generated/index.js
+++ src/rest-scratch-node-express/generated/index.js
@@ -3,16 +3,11 @@ const logger = require('./logger');
 const ExpressServer = require('./expressServer');
 
 const launchServer = async () => {
-  try {
-    console.log(`__dirname = ${__dirname}`)
-    console.log(`config.OPENAPI_YAML = ${config.OPENAPI_YAML}`)
-    this.expressServer = new ExpressServer(config.URL_PORT, config.OPENAPI_YAML);
-    this.expressServer.launch();
-    logger.info('Express server running');
-  } catch (error) {
-    logger.error('Express Server failure', error.message);
-    await this.close();
-  }
+  console.log(`__dirname = ${__dirname}`)
+  console.log(`config.OPENAPI_YAML = ${config.OPENAPI_YAML}`)
+  this.expressServer = new ExpressServer(config.URL_PORT, config.OPENAPI_YAML);
+  this.expressServer.launch();
+  logger.info('Express server running');
 };
 
 launchServer().catch(e => logger.error(e));
