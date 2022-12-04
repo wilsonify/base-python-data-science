20a5337937483ba495a6c3d14fe2de08a5db7dc5
wilsonify <tom.andrew.wilson@gmail.com>
2022-12-04 15:53:10 -0500

    add yaml

---
+++ src/rest-scratch-pistache/generate.sh
@@ -0,0 +1,7 @@
+docker run --rm \
+  -u $(id -u):$(id -g) \
+  -v "${PWD}:/local" \
+  openapitools/openapi-generator-cli:latest generate \
+  -i /local/openapi/openapi.yaml \
+  -g cpp-pistache-server \
+  -o /local/generated
