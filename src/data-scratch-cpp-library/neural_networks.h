de405eb9d00fab5fe8b82e690d9b6108f2d4a020
wilsonify <tom.a.wilson@gmail.com>
2022-12-11 10:46:03 -0500

    create some headers

---
+++ src/data-scratch-cpp-library/neural_networks.h
@@ -0,0 +1,14 @@
+
+#include "linear_algebra.h"
+
+double step_function(x) ;
+
+double perceptron_output(weights, bias, x) ;
+
+double sigmoid(t) ;
+
+double neuron_output(weights, inputs) ;
+
+double feed_forward(neural_network, input_vector) ;
+
+double backpropagate(network, input_vector, target) ;
