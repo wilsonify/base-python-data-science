5d9329d3954b6b71d882f9559a3dba518a381ce0
wilsonify <tom.a.wilson@gmail.com>
2022-12-11 11:50:29 -0500

    def main for stripheaders

--- src/data-scratch-cpp-library/strip_header.py
+++ src/data-scratch-cpp-library/strip_header.py
@@ -15,7 +15,13 @@ def source_to_header(source_str):
     return result
 
 
-for f in glob.glob("*.h"):
-    test_str = open(f, "r").read()
-    test_result_str = source_to_header(test_str)
-    print(test_result_str)
+def main():
+    for f in glob.glob("*.h"):
+        test_str = open(f, "r").read()
+        test_result_str = source_to_header(test_str)
+        with open(f, 'w') as output:
+            output.writelines(test_result_str)
+
+
+if __name__ == "__main__":
+    main()
