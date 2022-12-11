77c7b3286f3374f46349e9306b75f4e94a667858
wilsonify <tom.a.wilson@gmail.com>
2022-12-11 11:47:08 -0500

    cleanup headers

--- src/data-scratch-cpp-library/simple_linear_regression.h
+++ src/data-scratch-cpp-library/simple_linear_regression.h
@@ -1,23 +1,10 @@
-from dsl.stats import (
-    mean,
-    correlation,
-    standard_deviation,
-    de_mean,
-)
-
+#include "stats.h"
 
 double predict(alpha, beta, x_i) ;
-
 double error(alpha, beta, x_i, y_i) ;
-
 double sum_of_squared_errors(alpha, beta, x, y) ;
-
 double least_squares_fit(x, y) ;
-
 double total_sum_of_squares(y) ;
-
 double r_squared(alpha, beta, x, y) ;
-
 double squared_error(x_i, y_i, theta) ;
-
 double squared_error_gradient(x_i, y_i, theta) ;
--- src/data-scratch-cpp-library/stats.h
+++ src/data-scratch-cpp-library/stats.h
@@ -1,42 +1,16 @@
-import math
-from collections import Counter
-
-from dsl.linear_algebra import sum_of_squares, dot, shape, get_column, make_matrix
-
+#include "linear_algebra.h"
 
 double bucketize( double point, double bucket_size) ;
-
 double make_histogram(std::vector<double> points, double bucket_size) ;
-
 double correlation_matrix( std::vector<std::vector<double>> data) ;
-    return make_matrix(num_columns, num_columns, matrix_entry)
-}
-
-// this isn't right if you don't from __future__ import division
 double mean(std::vector<double> x) ;
-
 double median(std::vector<double> v) ;
-
 double quantile( std::vector<double> x, double p) ;
-
 double mode(std::vector<double> x) ;
-// "range" already means something in Python, so we'll use a different name
 double data_range(std::vector<double> x) ;
 double de_mean(std::vector<double> x) ;
-
 double variance(std::vector<double> x) ;
-
 double standard_deviation( std::vector<double> x) ;
-
 double interquartile_range( std::vector<double> x) ;
-
-////////
-//
-// CORRELATION
-//
-//////////
-
-
 double covariance(x, y) ;
-
 double correlation( std::vector<double> x, std::vector<double> y) ;
