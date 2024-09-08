ab94002d4d088a2ae9840afd30f16ed26a2cd790
wilsonify <tom.andrew.wilson@gmail.com>
2024-09-08 08:52:51 -0400

    test_data_science_from_scratch.py

--- src/data-scratch-matplotlib/data_plots_from_scratch/plot_state_borders.py
+++ src/data-scratch-matplotlib/data_plots_from_scratch/plot_state_borders.py
@@ -1,10 +1,11 @@
 import re
+from os.path import abspath, dirname
 
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 
-def read_lines(fn="states.txt"):
-    with open(fn, "r") as f:
+def read_lines(file_path):
+    with open(file_path, "r") as f:
         lines = [line for line in f]
     return lines
 
@@ -31,6 +32,8 @@ def plot_state_borders(segments, color="0.8"):
 
 
 if __name__ == "__main__":
-    lns = read_lines(fn="states.txt")
+    current_dir = abspath(dirname(__file__))
+    data_dir = abspath(f"{current_dir}/../../../data")
+    lns = read_lines(file_path=f"{data_dir}/states.txt")
     sgmts = lines_to_segments(lns)
     plot_state_borders(segments=sgmts)
