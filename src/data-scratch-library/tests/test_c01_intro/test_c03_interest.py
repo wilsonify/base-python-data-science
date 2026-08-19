d65363d5a96b3767a000b922b930318c8cf77191
wilsonify <tom.andrew.wilson@gmail.com>
2026-08-19 09:29:19 -0400

    Refactor interest attribute in Interest class tests for consistency

--- src/data-scratch-library/tests/test_c01_intro/test_c03_interest.py
+++ src/data-scratch-library/tests/test_c01_intro/test_c03_interest.py
@@ -3,18 +3,18 @@ from dsl.c01_intro import Interest
 
 def test_interest_creation():
     # Create an interest instance
-    interest = Interest(user_id=1, interest='Reading')
+    interest = Interest(user_id=1, interest_name='Reading')
 
     # Check that the attributes are set correctly
     assert interest.user_id == 1
-    assert interest.interest == 'Reading'
+    assert interest.interest_name == 'Reading'
 
 
 def test_interest_equality():
-    interest1 = Interest(user_id=1, interest='Reading')
-    interest2 = Interest(user_id=1, interest='Reading')
-    interest3 = Interest(user_id=2, interest='Reading')
-    interest4 = Interest(user_id=1, interest='Traveling')
+    interest1 = Interest(user_id=1, interest_name='Reading')
+    interest2 = Interest(user_id=1, interest_name='Reading')
+    interest3 = Interest(user_id=2, interest_name='Reading')
+    interest4 = Interest(user_id=1, interest_name='Traveling')
 
     # Test that two interests with the same attributes are considered equal
     assert interest1 == interest2
